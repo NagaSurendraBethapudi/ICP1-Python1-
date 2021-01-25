97787f3cfe0122b6b72cc378cf524b4aef4a5708
Naga Surendra Bethapudi <77935279+NagaSurendraBethapudi@users.noreply.github.com>
2021-01-24 18:03:29 -0600

    Update Question2-1.py

--- Question2-1.py
+++ Question2-1.py
@@ -1,3 +1,6 @@
 Name = input("Enter your Name :")
 print("Nice to meet you", Name)
 print("Name after removing the two characters and reversing the characters : " ,Name[5::-1])
+## However we can write this as removing two characters first and reversing the strings then.
+## Name[:-2]
+## Name [::-1]
